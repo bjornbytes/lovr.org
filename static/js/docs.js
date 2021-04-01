08a1d9958a5f991874ce9d6825728e5224149339
bjorn <tie.372@gmail.com>
2021-03-31 20:21:08 -0600

    Start fixing link controls;

--- static/js/docs.js
+++ static/js/docs.js
@@ -153,11 +153,11 @@ oboe('/api' + (window.location.pathname.match(/\/docs(?:\/v[\d\.]+|\/master)?/))
     var a = sidebar.querySelector('a[data-key="' + key + '"]');
     if (a) {
       var onclick = function(event) {
+        event.preventDefault();
         var content = document.querySelector('.content');
         if (content && content.dataset.key === key) { return; }
         pushPage(key);
         showPage(key, 0);
-        event.preventDefault();
       };
 
       a.classList.remove('disabled');
